de2aca8c42c826302aaea349590f2542a68fe776
Eileen S <eileen.j.sul@gmail.com>
2019-03-23 12:43:25 -0500

    new changes

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,8 +5,7 @@ function Nav() {
     <div class="navbar-fixed">
     <nav>
     <div className="nav-wrapper #4db6ac teal lighten-2">
-    <i class="fab fa-stripe-s"></i>
-      <a href="/" className="brand-logo">enior<b>N</b>ext<b>D</b>oor</a>
+      <a href="/" className="brand-logo"><b>S</b>enior<b>N</b>ext<b>D</b>oor</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="badges.html">Components</a></li>
         <li><a href="collapsible.html">JavaScript</a></li>
