3e6425ad5156a40634d96becbe2bf585cfc2c3e0
Yaye Fatou Thiam <yayefatouthiam@Yayes-Air.hsd1.il.comcast.net>
2019-03-29 23:06:26 -0500

    after merge with master

---
+++ oldclient3/src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+const Banner = () => {
+    return (
+        <div className="hero-image animated bounceInLeft">
+        </div>
+    );
+};
+
+export default Banner;
