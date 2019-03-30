bfb05ede44b0b6e1a62ff8d94a19581008459abd
Yaye Fatou Thiam <yayefatouthiam@Yayes-Air.hsd1.il.comcast.net>
2019-03-29 20:06:58 -0500

    after save google correction

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -28,8 +28,10 @@ module.exports = {
 
   // },
   create: function (req, res) {
+
+    console.log("data: "+req.body.email);
     db.Google
-      .createCollection(req.body)
+      .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
