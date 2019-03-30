84c536731f46c9690159a09bfd1c8529fe97a8c2
Yaye Fatou Thiam <yayefatouthiam@Yayes-Air.hsd1.il.comcast.net>
2019-03-29 21:00:26 -0500

    before merge with master

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -17,8 +17,25 @@ export default {
   saveService: function(serviceData) {
     return axios.post("/api/services", serviceData);
   },
+
+  // Saves a service to the database
+  emailService: function(serviceData) {
+    return axios.post("api/services/sendemail/mail", serviceData);
+  },
+  
+  // Gets all google users
+  getGoogle: function() {
+    return axios.get("/api/googlesignin");
+  },
   // Save Google profile information to the database
   saveGoogle: function(googleData) {
     return axios.post("/api/googlesignin", googleData);
   }
+
+
+
+
+
+
+  
 };
