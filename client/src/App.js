1f9278598c92082698d791fd290f33e6d0d17d60
Dara9234 <dahmed@alumni.depaul.edu>
2019-04-01 23:31:40 -0500

    Calendar working properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 // import './App.css';
 
-import SearchForm from './components/SearchForm';
-import Navbar from './components/Navbar';
+// import SearchForm from './components/SearchForm';
+// import Navbar from './components/Navbar';
 
 class App extends React.Component {
  
 
 render() {
   return (
-    <div className="App">
-      <Navbar />
-      <SearchForm />
-      
-     
-    
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/books" component={Landing} />
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import Jumbotron from "../components/Jumbotron";
+import SignUpBtn from "../components/SignUpBtn";
+import Footer from "../components/Footer"
+
+class Landing extends Component {
+    render(){
+        return(
+        <div>
+            <Jumbotron>SeniorNextDoor</Jumbotron>
+            <Footer />
+        </div>
+        );
+    }
+};
+
+export default Landing;
