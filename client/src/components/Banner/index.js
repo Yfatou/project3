7f400d51b69d23542779fceee641236a9af5cad1
sahmed <sahmed@alumni.depaul.edu>
2019-04-03 16:51:03 -0500

    How It Works button done

--- client/src/components/Banner/index.js
+++ client/src/components/Banner/index.js
@@ -5,7 +5,7 @@ import "./style.css"
 const Banner = () => {
     return (
         <div className="hero-image animated bounceInLeft">
-            
+         <a href="/howitworks" target="new" style= {{float:'right', marginTop:'5%'}} className="btn btnPrimary"> How It Works</a>
         </div>
     );
 };
