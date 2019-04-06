883c4c192347afccc37b5a2aadfbb958528c1167
Yaye Fatou Thiam <yayefatouthiam@Yayes-Air.hsd1.il.comcast.net>
2019-04-05 23:44:18 -0500

    deploy

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -15,4 +15,4 @@ export function ListItem(props) {
    //below are the actual attributes from <List> tag to be display as values that were stored in the attributes
   return <li className="list-group-item" data-id={props.key}>{" Title: "  + props.title } <br/> {"Zip: "+  props.zip}  <br/> {"Date: "+ props.date} <br/>{"Time: "+props.time } <br/> {"Notes: "+props.notes} <br/> {<a  className="btn btn-success"   href={"/send"} style={{color : "white"}}  >  I'M IN </a>}<br/></li>;
   // return <li className="list-group-item" data-id={props.key}>{" Title: "  + props.title } <br/> {"Zip: "+  props.zip}  <br/> {"Date: "+ props.date} <br/>{"Time: "+props.time } <br/> {"Notes: "+props.notes} <br/> <a href={"/UserInfos/" + props.profileId} target="_blank" rel="noopener noreferrer">Profile Link</a> <br/> {<a  className="btn btn-success"   href={"/send"} style={{color : "white"}}  >  I'M IN </a>}<br/></li>;
-}
+} 
