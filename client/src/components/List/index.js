4c5097cec758896c19520f2f13a3fa2130c581f9
Yaye Fatou Thiam <yayefatouthiam@Yayes-Air.hsd1.il.comcast.net>
2019-04-05 23:36:02 -0500

    deleted profile link

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -13,5 +13,6 @@ export function List({ children }) {
 //actual name of the attributes from FormSearchOpportunity.js-> <list> 
 export function ListItem(props) {  
    //below are the actual attributes from <List> tag to be display as values that were stored in the attributes
-  return <li className="list-group-item" data-id={props.key}>{" Title: "  + props.title } <br/> {"Zip: "+  props.zip}  <br/> {"Date: "+ props.date} <br/>{"Time: "+props.time } <br/> {"Notes: "+props.notes} <br/> <a href={"/UserInfos/" + props.profileId} target="_blank" rel="noopener noreferrer">Profile Link</a> <br/> {<a  className="btn btn-success"   href={"/send"} style={{color : "white"}}  >  I'M IN </a>}<br/></li>;
+  return <li className="list-group-item" data-id={props.key}>{" Title: "  + props.title } <br/> {"Zip: "+  props.zip}  <br/> {"Date: "+ props.date} <br/>{"Time: "+props.time } <br/> {"Notes: "+props.notes} <br/> {<a  className="btn btn-success"   href={"/send"} style={{color : "white"}}  >  I'M IN </a>}<br/></li>;
+  // return <li className="list-group-item" data-id={props.key}>{" Title: "  + props.title } <br/> {"Zip: "+  props.zip}  <br/> {"Date: "+ props.date} <br/>{"Time: "+props.time } <br/> {"Notes: "+props.notes} <br/> <a href={"/UserInfos/" + props.profileId} target="_blank" rel="noopener noreferrer">Profile Link</a> <br/> {<a  className="btn btn-success"   href={"/send"} style={{color : "white"}}  >  I'M IN </a>}<br/></li>;
 }
